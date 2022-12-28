de0ad5eaf454fa9ae2948e45b0038b6f2d12b495
Taufik <fikidram@gmail.com>
2022-12-28 14:20:42 +0700

    modif

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,6 +1,10 @@
+import Link from 'next/link';
 export default function Index() {
   return (
     <div>
+      <Link href="/about" title="About Page">
+      <a>About page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   )
